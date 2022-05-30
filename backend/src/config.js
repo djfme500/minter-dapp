942f54fad09ed39fdeba7b68fd3d54e932cc5b47
Daniel John <tan.daniel.john@gmail.com>
2022-05-30 11:08:07 +0700

    ok

--- backend/src/config.js
+++ backend/src/config.js
@@ -64,7 +64,7 @@ const PUBLIC_MINT_START_DATE = "2022-06-03T11:30:48+00:00"; // This is required.
 
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
 const PRESALE_MINT_START_DATE = "2022-05-30T11:30:48+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
-const ROYALTY_SHARE = 500; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
+const ROYALTY_SHARE = 600; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
 const ROYALTY_ADDRESS = "0xb69fcdE23b9673944Ddf9F61E40f2bc09d2a357B"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
 const PREREVEAL_TOKEN_URI = null; // only update if you want to manually set the prereveal token uri
